825da4abed297e075c9cf9eb4f79800afb8c35b1
warmonkey <luoshumymail@gmail.com>
2022-05-23 16:42:35 +0800

    Update TLE5012_test.ino
    arduino pin names corrected

--- examples/tle5012_test/TLE5012_test.ino
+++ examples/tle5012_test/TLE5012_test.ino
@@ -1,7 +1,7 @@
 #include <TLE5012.h>
 
 unsigned char ssc_delay = 0; // SSC clock frequency adjust, 0 = fastest, 255 = slowest
-TLE5012 enc(D8, D9, D10, ssc_delay);
+TLE5012 enc(8, 9, 10, ssc_delay);
 
 void setup() {
     Serial.begin(115200);
@@ -9,11 +9,11 @@ void setup() {
 }
 
 void loop() {
-    unsigned int angle = enc.readAngle();       //took 1ms on ATmega328P
+    unsigned int angle = enc.readAngle();
     Serial.print("Angle: ");
-    if(angle == enc::invalidAngle) {
+    if(angle == TLE5012::invalidAngle) {
         Serial.println("invalid");
     } else {
-        Serial.println(x);
+        Serial.println(angle);
     }
 }
